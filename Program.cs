4f9f3d9e2006c15c3eb2a6ac5cb1eca7df5aab89
Ben Hoberg <benjhoberg@gmail.com>
2023-03-02 09:05:56 -0600

    phase 3 complete

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-
 // Every class in the program is defined within the "Quest" namespace
 // Classes within the same namespace refer to one another without a "using" statement
 namespace Quest
@@ -9,6 +8,7 @@ namespace Quest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
             // Create a few challenges for our Adventurer's quest
             // The "Challenge" Constructor takes three arguments
             //   the text of the challenge
@@ -19,10 +19,8 @@ namespace Quest
                 "What's the answer to life, the universe and everything?", 42, 25);
             Challenge whatSecond = new Challenge(
                 "What is the current second?", DateTime.Now.Second, 50);
-
             int randomNumber = new Random().Next() % 10;
             Challenge guessRandom = new Challenge("What number am I thinking of?", randomNumber, 25);
-
             Challenge favoriteBeatle = new Challenge(
                 @"Who's your favorite Beatle?
     1) John
@@ -32,19 +30,16 @@ namespace Quest
 ",
                 4, 20
             );
-
             // "Awesomeness" is like our Adventurer's current "score"
             // A higher Awesomeness is better
-
             // Here we set some reasonable min and max values.
             //  If an Adventurer has an Awesomeness greater than the max, they are truly awesome
             //  If an Adventurer has an Awesomeness less than the min, they are terrible
             int minAwesomeness = 0;
             int maxAwesomeness = 100;
-
             // Make a new "Adventurer" object using the "Adventurer" class
+            // read line is a method in system, like a function in js
             Adventurer theAdventurer = new Adventurer(Console.ReadLine());
-
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
             List<Challenge> challenges = new List<Challenge>()
@@ -55,27 +50,29 @@ namespace Quest
                 guessRandom,
                 favoriteBeatle
             };
-
-            // Loop through all the challenges and subject the Adventurer to them
-            foreach (Challenge challenge in challenges)
+            string response = "yes";
+            while (response == "yes")
             {
-                challenge.RunChallenge(theAdventurer);
-            }
-
-            // This code examines how Awesome the Adventurer is after completing the challenges
-            // And praises or humiliates them accordingly
-            if (theAdventurer.Awesomeness >= maxAwesomeness)
-            {
-                Console.WriteLine("YOU DID IT! You are truly awesome!");
-            }
-            else if (theAdventurer.Awesomeness <= minAwesomeness)
-            {
-                Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
-            }
-            else
-            {
-                Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                foreach (Challenge challenge in challenges)
+                {
+                    challenge.RunChallenge(theAdventurer);
+                }
+                if (theAdventurer.Awesomeness >= maxAwesomeness)
+                {
+                    Console.WriteLine("YOU DID IT! You are truly awesome!");
+                }
+                else if (theAdventurer.Awesomeness <= minAwesomeness)
+                {
+                    Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+                }
+                else
+                {
+                    Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                }
+                Console.WriteLine("Would you like to repeat the quest?");
+                response = Console.ReadLine().ToLower();
             }
         }
     }
 }
+
